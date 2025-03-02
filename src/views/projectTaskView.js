21fb728945d512efae5839b5ec6cfd4df56a3a25
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-02 18:56:08 +0300

    adding methode update and update constructor

--- src/views/projectTaskView.js
+++ src/views/projectTaskView.js
@@ -1,15 +1,20 @@
 import View from './view'
 import Task from './taskView'
+import { NewTaskModal } from './modalView'
 
 export default class ProjectPlanTask extends View {
-  constructor(container, projectName, tasks) {
+  constructor(container, projectName) {
     super(container)
     this.projectName = projectName
-    console.log(tasks)
-    this.tasks = tasks
+    this.tasks = JSON.parse(localStorage.getItem('db')).find(
+      (obj) => obj.projectName === projectName
+    ).tasks
   }
+
   show() {
-    this.render(`<h1>Tasks for ${this.projectName}</h1>`)
+    this.render(`<h1>Tasks for ${this.projectName}</h1>
+      <button id='btn-new-task'>New task</button>
+      `)
     this.tasks.forEach((task, index) => {
       const div = document.createElement('div')
       div.classList.add('task')
@@ -24,5 +29,25 @@ export default class ProjectPlanTask extends View {
         task.priority
       ).show()
     })
+    const btnNewTask = document.getElementById('btn-new-task')
+    const createNewTask = new NewTaskModal(this)
+    btnNewTask.addEventListener('click', () => {
+      createNewTask.show()
+    })
+  }
+
+  update(taskObj) {
+    const div = document.createElement('div')
+    div.classList.add('task')
+    div.setAttribute('id', `task-${this.tasks.lenght}`)
+    this.append(div)
+    new Task(
+      `#task-${this.tasks.lenght}`,
+      this.projectName,
+      taskObj.title,
+      taskObj.description,
+      taskObj.date,
+      taskObj.priority
+    ).show()
   }
 }
