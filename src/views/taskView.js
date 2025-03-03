b3ffb95e4e850bf9a754ebfe71df85d42c70d054
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-03 22:17:49 +0300

    refactoring code

--- src/views/taskView.js
+++ src/views/taskView.js
@@ -1,16 +1,19 @@
+import { TaskModal as EditTaskModal } from './modalView'
 import View from './view'
 
 export default class Task extends View {
-  constructor(
+  constructor({
     container,
+    id,
     projectName,
     title,
     description,
     date,
     priority,
-    status = false
-  ) {
+    status = false,
+  }) {
     super(container)
+    this.id = id
     this.projectName = projectName
     this.title = title
     this.description = description
@@ -20,20 +23,42 @@ export default class Task extends View {
   }
 
   show() {
-    this.render(`
-        <span class="task-title">${this.title}<span>
-        <span class="task-description">${this.description}</span>
-        <span class="task-date">${this.date}</span>
-        <span class="task-priority">${this.priority}</span>
-        <span class="task-status">${this.status}</span>
-    `)
+    this.update()
+    const btnEdit = document.getElementById(`btn-edit-${this.id}`)
+    btnEdit.addEventListener('click', () => {
+      new EditTaskModal({
+        projectName: this.projectName,
+        id: this.id,
+        taskTitle: this.title,
+        taskDescription: this.description,
+        taskDate: this.date,
+        taskPriority: this.priority,
+      })
+        .show()
+        .then((result) => {
+          if (result) {
+            this.edit(result)
+          }
+        })
+    })
   }
 
-  edit(newTitle, newDescription, newDate, newPriority, newStatus) {
-    this.title = newTitle
-    this.description = newDescription
-    this.date = newDate
-    this.priority = newPriority
-    this.status = newStatus
+  edit(editedTask = {}) {
+    this.title = editedTask.title
+    this.description = editedTask.description
+    this.date = editedTask.date
+    this.priority = editedTask.priority
+    this.update()
+  }
+
+  update() {
+    this.render(`
+      <span class="task-title">${this.title}<span>
+      <span class="task-description">${this.description}</span>
+      <span class="task-date">${this.date}</span>
+      <span class="task-priority">${this.priority}</span>
+      <span class="task-status">${this.status}</span>
+      <button class='btn-edit' id='btn-edit-${this.id}' >Edit</button>
+      `)
   }
 }
