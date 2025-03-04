bb8855fe7c0383b16325df4c11c6fa6a1dcc5b18
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 10:29:33 +0300

    init all classes todotoplay donetask forgottentask and projectlist

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import '@fortawesome/fontawesome-free'
+import db from './db.json'
+import './style.css'
+import ToDoToday from './views/todayView'
+import ProjectList from './views/projectListView'
+import ForgottenTask from './views/forgottenView'
+import DoneTask from './views/doneView'
+
+localStorage.setItem('db', JSON.stringify(db))
+new ToDoToday('#view-container').show()
+new DoneTask('#view-container')
+new ForgottenTask('#view-container')
+new ProjectList('#project-container')
