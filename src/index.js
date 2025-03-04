59e75728f2977010cf5b17a0b8437352acb798b3
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 16:44:05 +0300

    add utilities and update controll method index .js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import ToDoToday from './views/todayView'
 import ProjectList from './views/projectListView'
 import ForgottenTask from './views/forgottenView'
 import DoneTask from './views/doneView'
+import { updateNotificationNumber } from './utils'
 
 localStorage.setItem('db', JSON.stringify(db))
 new ToDoToday('#view-container').show()
 new DoneTask('#view-container')
 new ForgottenTask('#view-container')
 new ProjectList('#project-container')
+updateNotificationNumber()
