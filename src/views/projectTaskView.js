b3078cb030f6923e6e0dafb429b6e1ce1f3d420c
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 10:25:12 +0300

    refactorign code and create a new TaskModal for each btn

--- src/views/projectTaskView.js
+++ src/views/projectTaskView.js
@@ -1,53 +1,50 @@
 import View from './view'
 import Task from './taskView'
-import { NewTaskModal } from './modalView'
+import { TaskModal as NewTaskModal } from './modalView'
 
 export default class ProjectPlanTask extends View {
-  constructor(container, projectName) {
+  constructor(container, projectID) {
     super(container)
-    this.projectName = projectName
-    this.tasks = JSON.parse(localStorage.getItem('db')).find(
-      (obj) => obj.projectName === projectName
-    ).tasks
+    const db = JSON.parse(localStorage.getItem('db')).find(
+      (obj) => obj.id == projectID
+    )
+    this.container = container
+    this.projectID = projectID
+    this.projectName = db.projectName
+    this.tasks = db.tasks
   }
 
   show() {
     this.render(`<h1>Tasks for ${this.projectName}</h1>
       <button id='btn-new-task'>New task</button>
       `)
-    this.tasks.forEach((task, index) => {
+    this.tasks.forEach((task) => {
       const div = document.createElement('div')
       div.classList.add('task')
-      div.setAttribute('id', `task-${index}`)
+      div.setAttribute('id', `task-${task.id}`)
       this.append(div)
-      new Task(
-        `#task-${index}`,
-        this.projectName,
-        task.title,
-        task.description,
-        task.date,
-        task.priority
-      ).show()
+      new Task({
+        container: `#task-${task.id}`,
+        projectName: this.projectName,
+        ...task,
+      }).show()
     })
-    const btnNewTask = document.getElementById('btn-new-task')
-    const createNewTask = new NewTaskModal(this)
-    btnNewTask.addEventListener('click', () => {
-      createNewTask.show()
-    })
-  }
 
-  update(taskObj) {
-    const div = document.createElement('div')
-    div.classList.add('task')
-    div.setAttribute('id', `task-${this.tasks.lenght}`)
-    this.append(div)
-    new Task(
-      `#task-${this.tasks.lenght}`,
-      this.projectName,
-      taskObj.title,
-      taskObj.description,
-      taskObj.date,
-      taskObj.priority
-    ).show()
+    const btnNewTask = document.getElementById('btn-new-task')
+    btnNewTask.addEventListener('click', () => {
+      const createNewTask = new NewTaskModal({ projectName: this.projectName })
+      createNewTask.show().then((result) => {
+        if (result) {
+          const div = document.createElement('div')
+          div.classList.add('task')
+          div.setAttribute('id', `task-${result.id}`)
+          this.append(div)
+          new Task({
+            container: `#task-${result.id}`,
+            ...result,
+          }).show()
+        }
+      })
+    })
   }
 }
