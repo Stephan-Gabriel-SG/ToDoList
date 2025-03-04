937e1c6f192261088cc74911faa451b09a27281a
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 16:41:54 +0300

    adding filter container

--- src/views/projectTaskView.js
+++ src/views/projectTaskView.js
@@ -1,7 +1,7 @@
 import View from './view'
 import Task from './taskView'
 import { TaskModal as NewTaskModal } from './modalView'
-import _ from 'lodash'
+import { applyFilterToTask } from '../utils'
 
 export default class ProjectPlanTask extends View {
   constructor(container, projectID) {
@@ -33,9 +33,10 @@ export default class ProjectPlanTask extends View {
     this.append(this.taskListContainer)
 
     const filterTask = document.getElementById('filter-task')
-    filterTask.addEventListener('change', () =>
-      this.applyFilter(filterTask.value)
-    )
+    filterTask.addEventListener('change', () => {
+      this.tasks = applyFilterToTask(filterTask.value, this.tasks)
+      this.showTasks()
+    })
 
     this.showTasks()
 
@@ -45,45 +46,11 @@ export default class ProjectPlanTask extends View {
       createNewTask.show().then((result) => {
         if (result) {
           this.tasks.push(result)
-          this.applyFilter(filterTask.value)
+          this.tasks = applyFilterToTask(filterTask.value, this.tasks)
+          this.showTasks()
         }
       })
     })
-  }
-
-  applyFilter(filter) {
-    const priorityOrder = { Urgent: 1, High: 2, Normal: 3 }
-    switch (filter) {
-      case 'date[A-Z]':
-        this.tasks = _.orderBy(
-          this.tasks,
-          [(task) => new Date(task.date)],
-          ['asc']
-        )
-        break
-      case 'date[Z-A]':
-        this.tasks = _.orderBy(
-          this.tasks,
-          [(task) => new Date(task.date)],
-          ['desc']
-        )
-        break
-      case 'priority[A-Z]':
-        this.tasks = _.orderBy(
-          this.tasks,
-          [(task) => priorityOrder[task.priority]],
-          ['asc']
-        )
-        break
-      case 'priority[Z-A]':
-        this.tasks = _.orderBy(
-          this.tasks,
-          [(task) => priorityOrder[task.priority]],
-          ['desc']
-        )
-        break
-    }
-    this.showTasks()
   }
 
   showTasks() {
