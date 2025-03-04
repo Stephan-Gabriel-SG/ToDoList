a60317e6c06bb1d92f204c565ca037948e6c2636
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 10:23:30 +0300

    add delete button and refactoring code

--- src/views/projectListView.js
+++ src/views/projectListView.js
@@ -1,6 +1,7 @@
 import View from './view'
 import ProjectPlanTask from './projectTaskView'
-import { NewProjectModal } from './modalView'
+import { ConfirmModal, ProjectModal as NewProjectModal } from './modalView'
+import { deleteProjectHandle } from '../controller'
 
 export default class ProjectList extends View {
   constructor(container) {
@@ -9,42 +10,77 @@ export default class ProjectList extends View {
     this.showProjectList()
     const addProject = document.getElementById('btn-new-project')
 
-    const newProjectModal = new NewProjectModal(this)
-    addProject.addEventListener('click', () => {
-      newProjectModal.show()
+    addProject.addEventListener('click', async () => {
+      const newProjectModal = new NewProjectModal({ edition: false })
+      newProjectModal.show().then((result) => {
+        if (result) {
+          this.add(result)
+        }
+      })
     })
   }
 
   showProjectList() {
     this.projectLists.forEach((projectObj) => {
-      const button = document.createElement('button')
-      button.textContent = projectObj.projectName
-      button.setAttribute('id', projectObj.projectName)
-      button.setAttribute('class', 'project-item')
-      button.addEventListener('click', () => {
-        new ProjectPlanTask(
-          '#view-container',
-          projectObj.projectName,
-          projectObj.tasks
-        ).show()
-      })
-      this.append(button)
+      this.add(projectObj)
     })
   }
 
-  update(newProjectObj) {
-    this.projectLists = Array.from(JSON.parse(localStorage.getItem('db')))
-    const button = document.createElement('button')
-    button.textContent = newProjectObj.projectName
-    button.setAttribute('id', newProjectObj.projectName)
-    button.setAttribute('class', 'project-item')
-    button.addEventListener('click', () => {
+  delete(projectID) {
+    document.getElementById(`project-${projectID}`)?.remove()
+    deleteProjectHandle(projectID)
+  }
+
+  add(newProjectObj) {
+    const projectContainer = document.createElement('div')
+    let projectName = newProjectObj.projectName
+    projectContainer.innerHTML = `
+      <span id='project-${newProjectObj.id}' class='project-title'>${newProjectObj.projectName}</span>
+      <button id='edit-${newProjectObj.id}'>Edit</button>
+      <button id='del-${newProjectObj.id}'>Delete</button>
+      
+    `
+    projectContainer.setAttribute('id', `project-${newProjectObj.id}`)
+    projectContainer.setAttribute('class', 'project-item')
+    projectContainer.addEventListener('click', () => {
       new ProjectPlanTask(
         '#view-container',
-        newProjectObj.projectName,
+        newProjectObj.id,
         newProjectObj.tasks
       ).show()
     })
-    this.append(button)
+    this.append(projectContainer)
+    document
+      .getElementById(`edit-${newProjectObj.id}`)
+      .addEventListener('click', () => {
+        const editProjectModal = new NewProjectModal({
+          ...newProjectObj,
+          projectName: projectName,
+          edition: true,
+        })
+        editProjectModal.show().then((result) => {
+          if (result) {
+            document.getElementById(`project-${newProjectObj.id}`).innerText =
+              result.projectName
+            projectName = result.projectName
+            new ProjectPlanTask('#view-container', result.id).show()
+          }
+        })
+      })
+    document
+      .getElementById(`del-${newProjectObj.id}`)
+      .addEventListener('click', () => {
+        new ConfirmModal({
+          modalTitle: 'Delete',
+          modalContent: `Would you like to delete ${newProjectObj.projectName} ?`,
+        })
+          .show()
+          .then((result) => {
+            if (result) {
+              this.delete(newProjectObj.id)
+              document.getElementById('today-task').click()
+            }
+          })
+      })
   }
 }
