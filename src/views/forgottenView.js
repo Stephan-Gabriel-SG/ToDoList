8771c53adedf69ac935f3a16bece48ae77ff079d
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 16:35:21 +0300

    refactoring and adding filter

--- src/views/forgottenView.js
+++ src/views/forgottenView.js
@@ -1,35 +1,65 @@
 import Task from './taskView'
 import View from './view'
 import { isPast, isToday } from 'date-fns'
-
+import { applyFilterToTask } from '../utils'
 export default class ForgottenTask extends View {
   constructor(container) {
     super(container)
-    this.btnToday = document.getElementById('forgotten-task')
-    this.btnToday.addEventListener('click', this.show.bind(this))
+    const notif = document.createElement('span')
+    notif.setAttribute('id', 'forgotten-notif')
+    this.btnForgotten = document.getElementById('forgotten-task')
+    this.btnForgotten.append(notif)
+    this.btnForgotten.addEventListener('click', this.show.bind(this))
   }
 
   show() {
-    this.render(`<h1>Forgotten task</h1>`)
+    this.render(`<h1>Forgotten task</h1>
+      <div>
+          <select id='filter-task' name='filter-task'>
+              <option value='date[A-Z]'>date [A-Z]</option>
+              <option value='date[Z-A]'>date [Z-A]</option>
+              <option value='priority[A-Z]'>priority [A-Z]</option>
+              <option value='priority[Z-A]'>priority [Z-A]</option>
+          </select>
+      </div>
+      <div id='task-list-container'></div>`)
+
+    this.taskListContainer = document.getElementById('task-list-container')
+    this.append(this.taskListContainer)
+
+    const filterTask = document.getElementById('filter-task')
+    filterTask.addEventListener('change', () => {
+      this.tasks = applyFilterToTask(filterTask.value, this.tasks)
+      this.showTasks()
+    })
+
     const db = JSON.parse(localStorage.getItem('db')) || []
-    db.forEach((projectObj) => {
-      for (let index = 0; index < projectObj.tasks.length; index++) {
-        if (
-          isPast(new Date(projectObj.tasks[index].date)) &&
-          !isToday(new Date(projectObj.tasks[index].date)) &&
-          !projectObj.tasks[index].status
-        ) {
-          const taskContent = document.createElement('div')
-          taskContent.setAttribute('id', `task-${index}`)
-          taskContent.classList.add('task')
-          this.append(taskContent)
-          new Task({
-            container: `#task-${index}`,
-            projectName: projectObj.projectName,
-            ...projectObj.tasks[index],
-          }).show()
-        }
-      }
+    this.tasks = db.flatMap((projectObj) =>
+      projectObj.tasks
+        .filter(
+          (task) =>
+            isPast(new Date(task.date)) &&
+            !isToday(new Date(task.date)) &&
+            !task.status
+        )
+        .map((task) => ({ ...task, projectName: projectObj.projectName }))
+    )
+
+    this.showTasks()
+  }
+
+  showTasks() {
+    this.taskListContainer.innerHTML = ''
+    this.tasks.forEach((task) => {
+      const div = document.createElement('div')
+      div.classList.add('task')
+      div.setAttribute('id', `task-${task.id}`)
+      this.taskListContainer.appendChild(div)
+      new Task({
+        container: `#task-${task.id}`,
+        projectName: task.projectName,
+        ...task,
+      }).show()
     })
   }
 }
