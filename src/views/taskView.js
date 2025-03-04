716733bbbaa5aec3f898494483e33f9658fc0235
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-04 16:43:17 +0300

    update task structure and adding checkbox to status

--- src/views/taskView.js
+++ src/views/taskView.js
@@ -1,4 +1,4 @@
-import { deleteTaskHandle } from '../controller'
+import { deleteTaskHandle, toggleTaskStatus } from '../controller'
 import { ConfirmModal, TaskModal as EditTaskModal } from './modalView'
 import View from './view'
 
@@ -25,37 +25,6 @@ export default class Task extends View {
 
   show() {
     this.update()
-    const btnEdit = document.getElementById(`btn-edit-${this.id}`)
-    btnEdit.addEventListener('click', () => {
-      new EditTaskModal({
-        projectName: this.projectName,
-        id: this.id,
-        taskTitle: this.title,
-        taskDescription: this.description,
-        taskDate: this.date,
-        taskPriority: this.priority,
-      })
-        .show()
-        .then((result) => {
-          if (result) {
-            this.edit(result)
-          }
-        })
-    })
-
-    const btnDelete = document.getElementById(`btn-delete-${this.id}`)
-    btnDelete.addEventListener('click', () => {
-      new ConfirmModal({
-        modalTitle: 'Delete',
-        modalContent: `Would you like to delete this task "${this.title}" ?`,
-      })
-        .show()
-        .then((result) => {
-          if (result) {
-            this.delete()
-          }
-        })
-    })
   }
 
   delete() {
@@ -77,9 +46,49 @@ export default class Task extends View {
       <span class="task-description">${this.description}</span>
       <span class="task-date">${this.date}</span>
       <span class="task-priority">${this.priority}</span>
-      <span class="task-status">${this.status}</span>
+      <label class="custom-checkbox">
+        <input type="checkbox" id='task-status-${this.id}' ${this.status ? 'checked' : ''}>
+        <span class="checkmark" ></span>
+        ${this.status}
+      </label>
       <button class='btn-edit' id='btn-edit-${this.id}' >Edit</button>
       <button class='btn-delete' id='btn-delete-${this.id}' >Delete</button>
       `)
+    const btnEdit = document.getElementById(`btn-edit-${this.id}`)
+    btnEdit.addEventListener('click', () => {
+      new EditTaskModal({
+        projectName: this.projectName,
+        id: this.id,
+        taskTitle: this.title,
+        taskDescription: this.description,
+        taskDate: this.date,
+        taskPriority: this.priority,
+      })
+        .show()
+        .then((result) => {
+          if (result) {
+            this.edit(result)
+          }
+        })
+    })
+    const btnDelete = document.getElementById(`btn-delete-${this.id}`)
+    btnDelete.addEventListener('click', () => {
+      new ConfirmModal({
+        modalTitle: 'Delete',
+        modalContent: `Would you like to delete this task "${this.title}" ?`,
+      })
+        .show()
+        .then((result) => {
+          if (result) {
+            this.delete()
+          }
+        })
+    })
+    const btnChangeStatus = document.getElementById(`task-status-${this.id}`)
+    btnChangeStatus.addEventListener('click', () => {
+      toggleTaskStatus(this.projectName, this.id)
+      this.status = !this.status
+      this.update()
+    })
   }
 }
