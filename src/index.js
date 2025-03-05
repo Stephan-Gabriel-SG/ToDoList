120084aa99d930da9d2a0d3ed4d55727be55ef15
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-03-05 19:01:51 +0300

    1-add activateMenu to update menu status. 2- add eventlistener for the toggle button slider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import '@fortawesome/fontawesome-free'
+// import 'hover.css/css/hover-min.css'
+import 'boxicons/css/boxicons.min.css'
+import '@fontsource-variable/inter'
 import db from './db.json'
 import './style.css'
 import ToDoToday from './views/todayView'
 import ProjectList from './views/projectListView'
 import ForgottenTask from './views/forgottenView'
 import DoneTask from './views/doneView'
-import { updateNotificationNumber } from './utils'
+import { activateMenu, updateNotificationNumber } from './utils'
 
 localStorage.setItem('db', JSON.stringify(db))
 new ToDoToday('#view-container').show()
@@ -13,3 +15,16 @@ new DoneTask('#view-container')
 new ForgottenTask('#view-container')
 new ProjectList('#project-container')
 updateNotificationNumber()
+
+activateMenu()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.querySelector('.sidebar-menu')
+  const toggleButton = document.getElementById('toggle-sidebar')
+  const container = document.getElementById('container')
+
+  toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-hidden')
+    container.classList.toggle('collapse-container')
+  })
+})
